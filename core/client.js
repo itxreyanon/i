6356e24389e0168527a41f8d751bf9be4aebe284
itxreyanon <itxreyanon@gmail.com>
2025-07-27 14:43:28 +0500

    Update client.js

--- core/client.js
+++ core/client.js
@@ -9,7 +9,7 @@ import ClientUser from '../structures/ClientUser.js';
 import Message from '../structures/Message.js';
 import Chat from '../structures/Chat.js';
 import User from '../structures/User.js';
-import Logger from '../utils/Utils.js';
+import Logger from '../utils/utils.js';
 
 
 /**
